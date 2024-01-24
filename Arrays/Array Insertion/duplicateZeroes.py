88e8f52172fc2857c4ef961aea7730fe9c44bb53
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-01-23 21:07:39 -0500

    Create duplicateZeroes.py

---
+++ Arrays/Array Insertion/duplicateZeroes.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: None Do not return anything, modify arr in-place instead.
+        """
+        i = 0
+        length = len(arr) - 1
+        while i < length:
+            number = arr[i]
+            if number == 0:
+                counter = length
+                while counter > i:
+                    arr[counter] = arr[counter - 1]     #right shift
+                    counter -= 1
+                if counter == i: #and counter != length:
+                    arr[counter + 1] = 0
+                    i+=1
+            i+=1
+
+"""
+Given a fixed-length integer array arr, duplicate each occurrence of zero,
+shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written.
+Do the above modifications to the input array in place and do not return anything.
+
+Example 1:
+
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+Example 2:
+
+Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+ 
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 9
+"""
