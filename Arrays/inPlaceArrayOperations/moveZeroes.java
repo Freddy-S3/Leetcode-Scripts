9537fba75356703f5d38d65fa68b6a949fa58ea7
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-02-12 17:02:20 -0500

    Create moveZeroes.java

---
+++ Arrays/inPlaceArrayOperations/moveZeroes.java
@@ -0,0 +1,42 @@
+package inPlaceArrayOperations;
+
+public class moveZeroes {
+    public void move(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[lastNonZeroFoundAt++] = nums[i];                 
+            }
+        }
+        for(; lastNonZeroFoundAt < nums.length; lastNonZeroFoundAt++){
+            nums[lastNonZeroFoundAt] = 0;
+        }
+    }
+}
+
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining 
+the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+ */
