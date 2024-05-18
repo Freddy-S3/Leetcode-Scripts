2556258dc9eda85e9ddbeed95e0a296b1ae88997
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-05-17 20:14:04 -0400

    Create DesignHashmap.py

---
+++ Hash Tables/HashTable/DesignHashmap.py
@@ -0,0 +1,78 @@
+class ListNode:
+    def __init__(self, key, val, nxt):
+        self.key = key
+        self.val = val
+        self.next = nxt
+class MyHashMap:
+    def __init__(self):
+        self.size = 19997
+        self.mult = 12582917
+        self.data = [None for _ in range(self.size)]
+    def hash(self, key):
+        return key * self.mult % self.size
+    def put(self, key, val):
+        self.remove(key)
+        h = self.hash(key)
+        node = ListNode(key, val, self.data[h])
+        self.data[h] = node
+    def get(self, key):
+        h = self.hash(key)
+        node = self.data[h]
+        while node:
+            if node.key == key: return node.val
+            node = node.next
+        return -1
+    def remove(self, key):
+        h = self.hash(key)
+        node = self.data[h]
+        if not node: return
+        if node.key == key:
+            self.data[h] = node.next
+            return
+        while node.next:
+            if node.next.key == key:
+                node.next = node.next.next
+                return
+            node = node.next
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+"""
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+ 
+
+Example 1:
+
+Input
+["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+Output
+[null, null, null, 1, -1, null, 1, null, -1]
+
+Explanation
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+ 
+
+Constraints:
+
+0 <= key, value <= 106
+At most 104 calls will be made to put, get, and remove.
+"""
