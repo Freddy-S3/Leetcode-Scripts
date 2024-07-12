13b8ea1394460fc9a109d6ef9acfd5a3f152483c
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-07-11 21:04:38 -0400

    Create ContainsDuplicateII.py

---
+++ Hash Tables/HashMap/ContainsDuplicateII.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic = {}
+        for i, v in enumerate(nums):
+            if v in dic and i - dic[v] <= k:
+                return True
+            dic[v] = i
+        return False
+        """
+        numberDict = {}
+        for i in range(len(nums)):
+            if nums[i] in numberDict:
+                if i - numberDict[nums[i]][-1] <= k:
+                    return True
+                else:
+                    numberDict[nums[i]].append(i)
+                
+            else:
+                numberDict[nums[i]] = [1,i]
+
+        return False
+        """
+
+
+"""
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+"""
