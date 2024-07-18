1778eba4cb6df16a1ab9459906dfe9116227f6df
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-07-18 18:01:58 -0400

    Create JewelsAndStones.py

---
+++ Hash Tables/AdvancedHashTables/JewelsAndStones.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        jewelset = set(jewels)
+        counter = 0
+        for stone in stones:
+            if stone in jewelset:
+                counter += 1
+        
+        return counter
+        #return sum(stone in jewelset for stone in stones)
+
+
+"""
+Jewels and Stones
+
+Solution
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+ 
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+   Hide Hint #1  
+For each stone, check if it is a jewel.
+"""
