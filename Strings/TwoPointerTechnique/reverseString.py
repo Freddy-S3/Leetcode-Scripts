93d04aab741046ccbe82cac0b4a6bd97376bfa0f
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-08-19 16:11:45 -0400

    Create reverseString.py

---
+++ Strings/TwoPointerTechnique/reverseString.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+            
+        return s
+
+
+"""
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+"""
