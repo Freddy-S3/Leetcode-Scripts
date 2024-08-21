c4b7c6b9af071b255bed9f36cf2155eccf142eca
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-08-21 15:58:46 -0400

    Create spiralMatrix.py

---
+++ Strings/twoDimensionalArrays/spiralMatrix.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        res = []
+        if len(matrix) == 0:
+            return res
+        row_begin = 0
+        col_begin = 0
+        row_end = len(matrix)-1 
+        col_end = len(matrix[0])-1
+        while (row_begin <= row_end and col_begin <= col_end):
+            for i in range(col_begin,col_end+1):
+                res.append(matrix[row_begin][i])
+            row_begin += 1
+            for i in range(row_begin,row_end+1):
+                res.append(matrix[i][col_end])
+            col_end -= 1
+            if (row_begin <= row_end):
+                for i in range(col_end,col_begin-1,-1):
+                    res.append(matrix[row_end][i])
+                row_end -= 1
+            if (col_begin <= col_end):
+                for i in range(row_end,row_begin-1,-1):
+                    res.append(matrix[i][col_begin])
+                col_begin += 1
+        return res
+        
+
+"""
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+"""
