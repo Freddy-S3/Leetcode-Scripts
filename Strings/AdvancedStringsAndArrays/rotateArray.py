da239aa76d67ccd40fd8cf0368542275d06a210c
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-08-28 13:29:09 -0400

    Create rotateArray.py

---
+++ Strings/AdvancedStringsAndArrays/rotateArray.py
@@ -0,0 +1,62 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        count = 0
+        start = 0
+        while count < len(nums):
+            current = start 
+            prev = nums[start] #store the value in the position
+            
+            while True:
+                next = (current + k) % len(nums) #
+                temp = nums[next] #store it temporaly 
+                nums[next] = prev #overide the next 
+                prev = temp #reset prev
+                current = next #reset current
+                count += 1
+
+                if start == current:
+                    break 
+            
+            start += 1
+
+"""
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+ 
+
+Follow up:
+
+Try to come up with as many solutions as you can. 
+There are at least three different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+"""
