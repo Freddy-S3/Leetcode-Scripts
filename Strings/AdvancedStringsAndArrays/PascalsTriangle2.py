74c847cf14d4836aae333f5d3b48bb24eb2ee239
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-11-04 20:51:04 -0500

    Create PascalsTriangle2.py

---
+++ Strings/AdvancedStringsAndArrays/PascalsTriangle2.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
+        return row
+
+"""
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+ 
+
+Constraints:
+
+0 <= rowIndex <= 33
+ 
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+"""
