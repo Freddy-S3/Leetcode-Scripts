8312c48b37670106b3b3454191e4a1d89dab2c8c
Freddy-S3 <141098248+Freddy-S3@users.noreply.github.com>
2024-11-12 20:17:37 -0500

    Create removeNthNodeFromEndOfList.py

---
+++ LinkedList/TwoPointerTechnique/removeNthNodeFromEndOfList.py
@@ -0,0 +1,81 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        fast, slow = head, head
+        
+        for _ in range(n): 
+            fast = fast.next
+        
+        if fast == None: 
+            return head.next
+        while fast.next:
+            fast, slow = fast.next, slow.next
+        
+        slow.next = slow.next.next
+        
+        return head 
+
+
+"""
+Given the head of a linked list, 
+remove the nth node from the end of the list and return its head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+ 
+
+Follow up: Could you do this in one pass?
+"""
+
+"""
+        if end.next == None:
+            if n == 1:
+                head = None
+                return head
+            else:
+                return head.next
+        else:
+            end = end.next
+        
+        while end.next != None:
+            end = end.next
+            header = header.next
+        
+        if n == 1:
+            header.next = None
+            
+        else:
+            header.next = header.next.next
+            
+        return head
+"""
